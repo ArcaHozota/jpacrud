65bebc5475bdac129f0f1f9005b07e08762e41e4
Arca Hozota <arkamahozota@gmail.com>
2023-09-25 17:55:00 +0900

    1.89

--- src/main/java/jp/co/toshiba/ppok/entity/Country.java
+++ src/main/java/jp/co/toshiba/ppok/entity/Country.java
@@ -2,14 +2,11 @@ package jp.co.toshiba.ppok.entity;
 
 import java.io.Serializable;
 import java.math.BigDecimal;
-import java.util.List;
 
-import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.NamedQuery;
-import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 import org.hibernate.annotations.Proxy;
@@ -126,10 +123,4 @@ public final class Country implements Serializable {
 	 */
 	@Column(nullable = false)
 	private String deleteFlg;
-
-	/**
-	 * This field corresponds to the database table LANGUAGE
-	 */
-	@OneToMany(mappedBy = "country", cascade = CascadeType.ALL)
-	private List<City> cities;
 }
