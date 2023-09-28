9143d9f9dfe5d17829de5c1c0c3445f705d5d399
Arca Hozota <arkamahozota@gmail.com>
2023-09-28 11:09:58 +0900

    2.06

--- src/main/java/jp/co/toshiba/ppok/dto/CityDto.java
+++ src/main/java/jp/co/toshiba/ppok/dto/CityDto.java
@@ -1,52 +1,11 @@
 package jp.co.toshiba.ppok.dto;
 
-import java.io.Serializable;
-
-import lombok.Data;
-
 /**
  * 都市情報DTO
  *
  * @author ArcaHozota
  * @since 1.07
  */
-@Data
-public final class CityDto implements Serializable {
-
-	private static final long serialVersionUID = 4831225402305424520L;
-
-	/**
-	 * This field corresponds to the database column ID
-	 */
-	private Long id;
-
-	/**
-	 * This field corresponds to the database column NAME
-	 */
-	private String name;
-
-	/**
-	 * This field corresponds to the database column CONTINENT
-	 */
-	private String continent;
-
-	/**
-	 * This field corresponds to the database column NATION
-	 */
-	private String nation;
-
-	/**
-	 * This field corresponds to the database column DISTRICT
-	 */
-	private String district;
-
-	/**
-	 * This field corresponds to the database column POPULATION
-	 */
-	private Long population;
-
-	/**
-	 * This field corresponds to the database column LANGUAGE
-	 */
-	private String language;
+public record CityDto(Long id, String name, String continent, String nation, String district, Long population,
+		String language) {
 }
